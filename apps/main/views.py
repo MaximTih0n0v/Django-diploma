072718c562b18535766f2480928896ac49777d99
Maxim Tihonov <badmax804@gmail.com>
2024-02-20 22:12:14 +0300

    refresh images, added pages index and about

--- apps/main/views.py
+++ apps/main/views.py
@@ -4,11 +4,18 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Главная страница сайта - Home',
+        'title': 'Home - Главная',
+        'content': 'Сервис аренды автомобилей',
+
     }
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse("About us")
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Мы не передаём Ваши личные данные третьим лицам'
+
+    }
+    return render(request, 'main/about.html', context)
