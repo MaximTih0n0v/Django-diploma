d38e32a2bca5552d62cad8da9e39e7f767f3a4da
Maxim Tihonov <badmax804@gmail.com>
2024-02-27 00:25:12 +0300

    added filter saves

--- apps/products/views.py
+++ apps/products/views.py
@@ -4,15 +4,24 @@ from apps.products.models import Products
 from django.shortcuts import get_object_or_404, render, get_list_or_404
 
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+    page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
 
     if category_slug == 'all':
         products = Products.objects.all()
     else:
         products = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
+    if on_sale:
+        products = products.filter(discount__gt=0)
+
+    if order_by and order_by != 'default':
+        products = products.order_by(order_by)
+
     paginator = Paginator(products, 3)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
     context = {
         'title': "Home - Каталог",
