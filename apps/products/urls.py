b8e8c397d9bc427caccbf758604b111eedeb4f00
Maxim Tihonov <badmax804@gmail.com>
2024-02-28 00:01:37 +0300

    feat: added search by id

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -5,6 +5,7 @@ from apps.products import views
 app_name = 'products'
 
 urlpatterns = [
-    path('<slug:category_slug>', views.catalog, name='index'),
+    path('search/', views.catalog, name='search'),
+    path('<slug:category_slug>/', views.catalog, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product'),
 ]
