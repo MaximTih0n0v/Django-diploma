e81a3fd4713fdb517d481c05223beb7bd8866ec6
Maxim Tihonov <badmax804@gmail.com>
2024-03-01 13:45:14 +0300

    feat: added the login form

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -4,18 +4,8 @@ from apps.users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.CharField(
-        label='Имя пользователя',
-        widget=forms.TextInput(attrs={'autofocus': True,
-                                      'class': 'form-control',
-                                      'placeholder': 'Введите имя пользователя'})
-    )
-    password = forms.CharField(
-        label='Пароль',
-        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password',
-                                          'class': 'form-control',
-                                          'placeholder': 'Введите Ваш пароль'})
-    )
+    username = forms.CharField()
+    password = forms.CharField()
 
     class Meta:
         model = User
